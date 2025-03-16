184b81504b7de5864cd0e0da8475b62419ba0cd7
GetALifehahaha <sencioahlannour@gmail.com>
2025-03-16 11:34:12 +0800

    refactor

--- Actual Proj/app.js
+++
@@ -1,4 +0,0 @@
-let qrCode = document.getElementById("qr_code");
-let schoolId = document.getElementById("")
-
-qrCode.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + 202300049;
--- Actual Proj/dbconnect.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $conn = new mysqli("localhost", "root", "", "Student_Attendance_DB") or die("Error Connection: " + mysqli_connect_error());
-?>
