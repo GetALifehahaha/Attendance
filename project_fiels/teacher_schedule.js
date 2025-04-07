be00c49264d8135ad5aff3f22d9bb44bc5cef341
Reneilrp <pheinz.magnun@gmail.com>
2025-04-07 23:49:17 +0800

    Removed Add Student modal and Updated Add schedule modal(still has flaw).

--- project_fiels/teacher_schedule.js
+++ project_fiels/teacher_schedule.js
@@ -13,5 +13,35 @@ close_schedule_form.addEventListener("click", function(){
     form.classList.toggle("show");
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+    const steps = document.querySelectorAll('.form-step');
+    let currentStep = 0;
 
-//hover effects
+    function showStep(stepIndex) {
+        steps.forEach((step, index) => {
+            step.classList.toggle('active', index === stepIndex);
+        });
+    }
+
+    document.getElementById('next-to-step-2').addEventListener('click', () => {
+        currentStep = 1;
+        showStep(currentStep);
+    });
+
+    document.getElementById('prev-to-step-1').addEventListener('click', () => {
+        currentStep = 0;
+        showStep(currentStep);
+    });
+
+    document.getElementById('next-to-step-3').addEventListener('click', () => {
+        currentStep = 2;
+        showStep(currentStep);
+    });
+
+    document.getElementById('prev-to-step-2').addEventListener('click', () => {
+        currentStep = 1;
+        showStep(currentStep);
+    });
+
+    showStep(currentStep);
+});
